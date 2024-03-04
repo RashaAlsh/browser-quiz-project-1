55459aa485a5ebbdb208cfb86f551ed451b7ca88
ibrahim ihsan şahin <etliekmek97@gmail.com>
2024-03-04 15:16:42 +0100

    feat: show correct/wrong answers

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -30,24 +30,54 @@ export const initQuestionPage = () => {
 
     // adds event listeners to all the answer elements. so user can select an answer
   for (const answerElement of answersListElement.children) {
-    answerElement.addEventListener('click', () => {
-      selectAnswer(answerElement);
-    });
-  }
-};
+    answerElement.addEventListener('click', selectEventHandler);
+  }  
+}
 
+
+// handler (I think)
+function selectEventHandler(){
+  selectAnswer(this);
+}
 
 // add active class to selected to answer
 const selectAnswer = (selectElement) => {
+  const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
   
-  for (const answer of selectElement.parentElement.children) {
-    answer.classList.remove('active');
+  // modify the data
+  currentQuestion.selected = selectElement.innerHTML.split(":")[0].trim();
+  
+  // remove the click events listeners since user answered already and we showed the answer
+  for (const answerElement of selectElement.parentElement.children) {
+    answerElement.removeEventListener('click', selectEventHandler);     
   }
-  selectElement.classList.add('active');  
+ 
+  // if the answer is wrong, add .wrong
+  if(!answerIsCorrect(currentQuestion)){
+    selectElement.classList.add('wrong');
+  }
+
+  // add .correct to the correct answer
+  showCorrectAnswer(currentQuestion);
+  
+}
+
+const answerIsCorrect = (question) => {
+  return question.selected === question.correct;
+}
+
+// color the correct answer to green
+const showCorrectAnswer = (question) => {
+  const answersList = document.getElementById(ANSWERS_LIST_ID);
+  for (const answerElement of answersList.children) {
+    if (answerElement.innerHTML.trim().startsWith(question.correct)) {
+      answerElement.classList.add('correct');
+    }
+  }
 }
 
 const nextQuestion = () => {
   quizData.currentQuestionIndex = quizData.currentQuestionIndex + 1;
 
   initQuestionPage();
-};
+}
