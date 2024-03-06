a723512c71f9de945761b4e9cd827b4e7ee7cc5d
Elias Soykat <eliasmd624@gmail.com>
2024-03-06 08:19:22 +0600

    fix: remove title from js code

--- src/pages/welcomePage.js
+++ src/pages/welcomePage.js
@@ -1,12 +1,10 @@
-import { USER_INTERFACE_ID, START_QUIZ_BUTTON_ID } from '../constants.js';
+import { START_QUIZ_BUTTON_ID, USER_INTERFACE_ID } from '../constants.js';
 import { createWelcomeElement } from '../views/welcomeView.js';
 import { initQuestionPage } from './questionPage.js';
 
 export const initWelcomePage = () => {
   const userInterface = document.getElementById(USER_INTERFACE_ID);
   userInterface.innerHTML = '';
-
-  document.title = "Quiz Application"
 
   const welcomeElement = createWelcomeElement();
   userInterface.appendChild(welcomeElement);
