4e3a8188ce7d5dd22411c58c455da6ef8d1821dd
hhana1-1 <hhana1@live.com>
2024-03-08 18:14:08 +0100

    feat/user input form

--- src/pages/welcomePage.js
+++ src/pages/welcomePage.js
@@ -2,32 +2,46 @@ import { START_QUIZ_BUTTON_ID, USER_INTERFACE_ID } from '../constants.js';
 import { createWelcomeElement } from '../views/welcomeView.js';
 import { initQuestionPage } from './questionPage.js';
 
-export const initWelcomePage = () => {
-  const userInterface = document.getElementById(USER_INTERFACE_ID);
-  userInterface.innerHTML = '';
+const userInterface = document.getElementById(USER_INTERFACE_ID);
 
+
+   
+let colors = ["#ccb4f5", "#b4f5ed", "#f5b4bc", "#dcf5b4"]; 
+// let colors = ["#b4f5ed", "#dcf5b4"]; 
+ let colorIndex = 1; 
+
+var intervalID = setInterval (() => {  
+   document.getElementById('start-quiz-button').style.backgroundColor = colors[colorIndex]; 
+   colorIndex = (colorIndex + 1) % colors.length; 
+ }, 300); 
+
+export const initWelcomePage = () => {
+  userInterface.innerHTML = '';
   const welcomeElement = createWelcomeElement();
   userInterface.appendChild(welcomeElement);
+
 
   document
     .getElementById(START_QUIZ_BUTTON_ID)
     .addEventListener('click', startQuiz);
+
+    
+    
+    
 };
 
-const startQuiz = () => {
-  initQuestionPage(); 
-};
-
-//start quiz button animation
-
-export function switchColor() {
-  let colors = ["#ccb4f5", "#b4f5ed", "#f5b4bc", "#dcf5b4"]; 
- // let colors = ["#b4f5ed", "#dcf5b4"]; 
-  let colorIndex = 0; 
-  setInterval(function() {
-    document.getElementById('start-quiz-button').style.backgroundColor = colors[colorIndex]; 
-    colorIndex = (colorIndex + 1) % colors.length; 
-  }, 300); 
+function saveName() {
+  let usernameValue = document.querySelector('input[name="userName"]').value;
+  localStorage.setItem('userName', usernameValue);
 }
 
-switchColor(); 
+
+const startQuiz = () => {
+  saveName();
+  initQuestionPage(); 
+  clearInterval(intervalID);
+};
+
+
+
+
